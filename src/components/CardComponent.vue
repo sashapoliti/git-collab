58f540c3bab910ccfa2f2a0421210d5a1b2c9187
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-04-12 12:51:14 +0200

    Card Riccardo M

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        card test
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CardComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
